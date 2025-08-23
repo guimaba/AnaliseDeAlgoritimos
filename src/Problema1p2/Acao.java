5084522b0755382b961deb3c47cc65f8aa549ad3
Guilherme Maba <guimaba2102@gmail.com>
2025-08-23 18:34:09 -0300

    parte2

---
+++ src/Problema1p2/Acao.java
@@ -0,0 +1,136 @@
+package Problema1p2;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Acao implements Subject{
+
+    private String nomeAcao;
+    private double valor;
+    private ArrayList<Ordem> listaOrdens = new ArrayList<>();
+    private ArrayList<Investidor> listaInvestidoresRegistrados = new ArrayList<>();
+    private ArrayList<Ordem> ordemPreProgramada = new ArrayList<>();
+    private ArrayList<Observer> observadores = new ArrayList<>();
+
+    public Acao(String nomeAcao, double valor) {
+        this.nomeAcao = nomeAcao;
+        this.valor = valor;
+    }
+
+    public Acao() {
+    }
+
+    public void inserirOrdem(Ordem ordem) {
+        listaOrdens.add(ordem);
+        verificarMatch();
+        ordemPre();
+    }
+
+    public void inserirOrdemProgramada(Ordem ordem) {
+        ordemPreProgramada.add(ordem);
+        System.out.println("Ordem pre cadastrada" + ordemPreProgramada);
+    }
+
+    public void ordemPre() {
+        for (Ordem ordem : ordemPreProgramada) {
+            if (ordem.getValor() == getValor()) {
+                listaOrdens.add(ordem);
+            }
+        }
+    }
+
+    public void inserirInvestidor(Investidor investidor) {
+        listaInvestidoresRegistrados.add(investidor);
+    }
+
+    public void verificarMatch() {
+        Iterator<Ordem> iterator = listaOrdens.iterator();
+        ArrayList<Ordem> ordemRemovidas = new ArrayList<>();
+        while (iterator.hasNext()) {
+            Ordem ordem = iterator.next();
+            for (Ordem ordemNext : listaOrdens) {
+                if (!ordem.equals(ordemNext) && !ordem.getTipo().equals(ordemNext.getTipo())
+                        && ordem.getValor() == ordemNext.getValor()) {
+                    ordemRemovidas.add(ordem);
+                    ordemRemovidas.add(ordemNext);
+                    iterator.remove();
+                    System.out.println("Valor da ultima negociacao: " + nomeAcao + " : " + ordem.getValor());
+                    atualizarValor(valor);
+                    break;
+                }
+            }
+        }
+        listaOrdens.removeAll(ordemRemovidas);
+    }
+
+    public void notificar() {
+        for (Investidor investidor : listaInvestidoresRegistrados) {
+            System.out.println(investidor.getNomeInvestidor() + " valor atualizado da acao: " + valor);
+        }
+    }
+
+    public void atualizarValor(double novoValor) {
+        novoValor += 5;
+        valor = novoValor;
+        notifyObservers();
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public ArrayList<Ordem> getListaOrdens() {
+        return listaOrdens;
+    }
+
+    @Override
+    public String toString() {
+        return nomeAcao + " = " + valor + " = " + listaOrdens;
+    }
+
+    public void setListaOrdens(ArrayList<Ordem> listaOrdens) {
+        this.listaOrdens = listaOrdens;
+    }
+
+    public String getNomeAcao() {
+        return nomeAcao;
+    }
+
+    public void setNomeAcao(String nomeAcao) {
+        this.nomeAcao = nomeAcao;
+    }
+
+    public ArrayList<Investidor> getListaInvestidoresNotificados() {
+        return listaInvestidoresRegistrados;
+    }
+
+    public void setListaInvestidoresNotificados(ArrayList<Investidor> listaInvestidoresNotificados) {
+        this.listaInvestidoresRegistrados = listaInvestidoresNotificados;
+    }
+
+    @Override
+    public void registerObserver(Observer observer) {
+        observadores.add(observer);
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        observadores.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (Observer o : observadores) {
+            o.update();
+        };
+    }
+
+    public ArrayList<Observer> getObservadores() {
+        return observadores;
+    }
+
+}
---
+++ src/Problema1p2/Investidor.java
@@ -0,0 +1,46 @@
+package Problema1p2;
+
+public class Investidor implements Observer {
+
+    private String nomeInvestidor;
+    public Acao acao;
+
+
+    public Investidor(String nomeInvestidor) {
+        this.nomeInvestidor = nomeInvestidor;
+    }
+
+    public String getNomeInvestidor() {
+        return nomeInvestidor;
+    }
+
+    public void setNomeInvestidor(String nomeInvestidor) {
+        this.nomeInvestidor = nomeInvestidor;
+    }
+
+    public void registrarOrdem(Acao acao, String tipo, double valor) {
+        Ordem ordem = new Ordem(new Investidor(this.nomeInvestidor), tipo, valor);
+        acao.inserirOrdem(ordem);
+    }
+
+    public void programarOrdem(Acao acao, String tipo, double valor) {
+        Ordem ordem = new Ordem(new Investidor(this.nomeInvestidor), tipo, valor);
+        acao.inserirOrdemProgramada(ordem);
+    }
+
+    public void registrarseEmUmaAcao(Acao acao){
+        acao.registerObserver(this);
+        this.acao = acao;
+        System.out.println(this.nomeInvestidor + " registrado com sucesso na acao = " + acao.getNomeAcao());
+    }
+
+    @Override
+    public String toString() {
+        return nomeInvestidor;
+    }
+
+    @Override
+    public void update() {
+            System.out.println(this.nomeInvestidor + " valor atualizado da acao: " + acao.getValor());
+    }
+}
